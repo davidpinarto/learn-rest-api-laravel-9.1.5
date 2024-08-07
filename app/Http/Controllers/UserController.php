69d5bbf46c8afc3f11a280db40071201f5ac4261
David Pinarto <davidpinarto90@gmail.com>
2024-08-07 15:04:04 +0800

    fix catch database with QueryException

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
@@ -43,6 +45,30 @@ class UserController extends Controller
                 'status' => 'fail',
                 'message' => $e->validator->errors()->first()
             ], 400);
+        } catch (QueryException $e) {
+            /**
+             * SQLSTATE[23505]: Unique violation: 7 ERROR:  duplicate key value violates unique constraint 
+             * \"users_email_unique\"\nDETAIL:  Key (email)=(davidpinarto90@gmail.com) already exists. (SQL: insert into \"users\" 
+             * (\"name\", \"email\", \"password\", \"id\", \"updated_at\", \"created_at\") values (david, davidpinarto90@gmail.com, 
+             * $2y$10$TdrTt0lYUDvOYuBKPEucf.kPUkZUUjw3I4CLvulhjN7aFORPQDWgm, user-x48ZtAipvcYnQIiQ, 2024-08-07 07:01:02, 2024-08-07 
+             * 07:01:02))
+             */
+            if ($e->getCode() == '23505') { // Unique constraint violation code
+                return response()->json([
+                    'status' => 'fail',
+                    'message' => 'The email address is already in use.'
+                ], 400);
+            }
+
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Database error: ' . $e->getMessage()
+            ], 400);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $e->getMessage()
+            ], 500);
         }
     }
 }
