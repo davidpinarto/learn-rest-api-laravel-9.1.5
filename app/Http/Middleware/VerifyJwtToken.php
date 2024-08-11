0a3a6dc00a3a68b8bb7562de6d80421a901a0cc9
David Pinarto <davidpinarto90@gmail.com>
2024-08-11 23:28:16 +0800

    refactor VerifyJwtToken middleware

--- app/Http/Middleware/VerifyJwtToken.php
+++ app/Http/Middleware/VerifyJwtToken.php
@@ -4,9 +4,13 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Exception;
+use Firebase\JWT\ExpiredException;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 use Illuminate\Http\Request;
+use UnexpectedValueException;
+use Firebase\JWT\SignatureInvalidException;
+use App\Exceptions\UnauthorizedException;
 
 class VerifyJwtToken
 {
@@ -21,14 +25,9 @@ class VerifyJwtToken
     {
         try {
             $token = $request->bearerToken();  // string of the token
-            // var_dump($token);
 
             if (!$token) {
-                $response = [
-                    'status' => 'fail',
-                    'message' => 'Missing token on the Bearer'
-                ];
-                return response()->json($response, 401);
+                throw new UnauthorizedException('Missing token on the Bearer');
             }
 
             $credentials = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
@@ -37,15 +36,34 @@ class VerifyJwtToken
                 'id' => $credentials->id,
                 'email' => $credentials->email
             ];
-            // var_dump($request->userData);
 
             return $next($request);
         } catch (Exception $e) {
+            if (
+                $e instanceof UnauthorizedException
+                || $e instanceof SignatureInvalidException
+                || $e instanceof UnexpectedValueException  // ketika user mengisi nilai sembarang pada Bearer token
+            ) {
+                $response = [
+                    'status' => 'fail',
+                    'message' => $e->getMessage(),
+                ];
+                return response()->json($response, 401);
+            }
+
+            if ($e instanceof ExpiredException) {
+                $response = [
+                    'status' => 'fail',
+                    'message' => 'Token is invalid or expired'
+                ];
+                return response()->json($response, 401);
+            }
+
             $response = [
                 'status' => 'fail',
-                'message' => 'Token is invalid or expired'
+                'message' => 'There is something error on our server',
             ];
-            return response()->json($response, 401);
+            return response()->json($response, 500);
         }
     }
 }
